dfaf6dea413abe869b4520cdffc1885ff5891782
DanielHadianto456 <daniel88hadianto@gmail.com>
2026-01-15 20:53:41 +0700

    feat: add loop and update logic

--- calculator.c
+++ calculator.c
@@ -1,62 +1,76 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ADDITION_OPTION '+'
 #define SUBTRACTION_OPTION '-'
 #define MULTIPLICATION_OPTION '*'
 #define DIVISION_OPTION '/'
+#define FINISHED_OPTION '='
 
 int main()
 {
   int inputtedNumber, currentTotal;
   char currentOperationOption;
+  bool isFinished = false;
 
   printf("\nInput your number here: ");
   scanf("%i", &currentTotal);
   
   getchar();
 
-  printf("\nInput your operation (Supported operations %c, %c, %c, %c): ", 
-         ADDITION_OPTION, 
-         SUBTRACTION_OPTION, 
-         MULTIPLICATION_OPTION, 
-         DIVISION_OPTION
-        );
-  currentOperationOption = getchar();
+  do {
+    printf("\nInput your operation (Supported operations %c, %c, %c, %c, %c): ", 
+           ADDITION_OPTION, 
+           SUBTRACTION_OPTION, 
+           MULTIPLICATION_OPTION, 
+           DIVISION_OPTION,
+           FINISHED_OPTION
+         );
+    currentOperationOption = getchar();
 
-  if (currentOperationOption != ADDITION_OPTION && 
-      currentOperationOption != SUBTRACTION_OPTION && 
-      currentOperationOption != DIVISION_OPTION && 
-      currentOperationOption != MULTIPLICATION_OPTION
-    ) {
-    printf("\nInvalid operation type selected!\n");
+    if (currentOperationOption != ADDITION_OPTION && 
+        currentOperationOption != SUBTRACTION_OPTION && 
+        currentOperationOption != DIVISION_OPTION && 
+        currentOperationOption != MULTIPLICATION_OPTION &&
+        currentOperationOption != FINISHED_OPTION
+      ) {
+      printf("\nInvalid operation type selected! Selected = %c\n", currentOperationOption);
 
-    return 0;
-  }
+      continue;
+    }
 
-  printf("\nInput your next number: ");
-  scanf("%i", &inputtedNumber);
-
-  switch (currentOperationOption) {
-    case ADDITION_OPTION:
-      currentTotal += inputtedNumber;
-
+    if (currentOperationOption == FINISHED_OPTION) {
+      isFinished = true;
       break;
+    }
 
-    case SUBTRACTION_OPTION:
-      currentTotal -= inputtedNumber;
+    printf("\nInput your next number: ");
+    scanf("%i", &inputtedNumber);
+
+    switch (currentOperationOption) {
+      case ADDITION_OPTION:
+        currentTotal += inputtedNumber;
+
+        break;
+
+      case SUBTRACTION_OPTION:
+        currentTotal -= inputtedNumber;
       
-      break;
-
-    case MULTIPLICATION_OPTION:
-      currentTotal *= inputtedNumber;
+        break;
+ 
+      case MULTIPLICATION_OPTION:
+        currentTotal *= inputtedNumber;
       
-      break;
+        break;
 
-    case DIVISION_OPTION:
-      currentTotal /= inputtedNumber;
+      case DIVISION_OPTION:
+        currentTotal /= inputtedNumber;
       
-      break;
-  }
+        break;
+    }
+
+    getchar();
+  } while(!isFinished); 
 
   printf("\n%i\n", currentTotal);
 
